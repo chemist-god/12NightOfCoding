4c1218b540edae2a1bbc37869a3ec1ba4fe26337
chemist-god <godtenkorang72@gmail.com>
2024-12-29 23:16:33 +0000

    function for setting the state variable value

---
+++ contracts/set_function.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity >= 0.4.16 < 0.9.0;
+/// @title A contract for demonstrate how to write a smart contract 
+/// @author Jitendra Kumar
+/// @notice For now, this contract just show to set the value of state variable and get this value from the smart contract
+contract Storage
+{
+
+	// Declaring state variables
+	uint public setData;
+
+	// Defining public function 
+	// that sets the value of 
+	// the state variable
+	function set(uint x) public
+	{
+		setData = x;
+	}
+	
+	// Defining function to
+	// print the value of
+	// state variable
+	function get(
+	) public view returns (uint) {
+		return setData;
+	}
+}
